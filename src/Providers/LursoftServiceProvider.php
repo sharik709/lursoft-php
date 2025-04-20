f8dd1651e91c6f4059731900eebad76ffa9e81f5
Sharik Shaikh <shaikhsharik709@gmail.com>
2025-04-20 10:57:35 +0530

    cache interface argument correction

--- src/Providers/LursoftServiceProvider.php
+++ src/Providers/LursoftServiceProvider.php
@@ -3,6 +3,9 @@
 namespace Sharik709\LursoftPhp\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Psr\SimpleCache\CacheInterface;
+use Illuminate\Contracts\Cache\Repository;
+use Illuminate\Support\Facades\Cache;
 use Sharik709\LursoftPhp\Services\LursoftService;
 
 class LursoftServiceProvider extends ServiceProvider
@@ -14,8 +17,22 @@ class LursoftServiceProvider extends ServiceProvider
         );
 
         $this->app->singleton(LursoftService::class, function ($app) {
+            // Get cache store that implements PSR-16 SimpleCache
+            $cache = $app->make(CacheInterface::class);
+
+            // If Laravel's cache isn't bound to CacheInterface, use Laravel's cache store
+            if (!$cache && $app->bound(Repository::class)) {
+                $cache = $app->make(Repository::class);
+            }
+
+            // Fallback to the default cache store
+            if (!$cache) {
+                $cache = Cache::store();
+            }
+
             return new LursoftService(
-                config('lursoft.base_url')
+                $cache,
+                config('lursoft.base_url', 'https://b2b.lursoft.lv')
             );
         });
     }
